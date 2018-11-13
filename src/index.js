a8ad89fb3862840fc422ac52717d0d9e5e0bab66
Ryan Lett <ryan.james.lett@gmail.com>
2018-11-13 11:47:35 -0800

    Initial page commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,61 @@
+// History Component
+// ===================================
+//
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Projects extends React.Component {
+  render() {
+    // TODO: loop over project listing data
+    return (
+      <div className="projects">
+        <div className="project-item">One</div>
+        <div className="project-item">Two</div>
+        <div className="project-item">Three</div>
+        <div className="project-item">Four</div>
+      </div>
+    )
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+class ProjectListing extends React.Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      // projects: getProjectData(),
+      title: "Project Listing",
+    }
+  }
+
+  render() {
+    return (
+      <div className="project-listing">
+        <h2 className="info-block">
+          {this.state.title}
+        </h2>
+        <Projects />
+      </div>
+    );
+  }
+}
+
+
+function Page(props) {
+  return (
+    <div>
+      <h1 className="page-header">RyanLett.com</h1>
+      <ProjectListing />
+    </div>
+  );
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Page />,
+  document.getElementById('root')
+);
+
