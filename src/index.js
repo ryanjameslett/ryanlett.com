2e9678dbb5cd7feb0ffc1bf9881cb093c45e2b99
Ryan Lett <ryan.james.lett@gmail.com>
2018-11-15 15:44:17 -0800

    Updated app to pull data from json file. Will come from a data server at some point.

--- src/index.js
+++ src/index.js
@@ -46,51 +46,32 @@ class Projects extends React.Component {
 }
 
 class ProjectListing extends React.Component {
-  getProjectData() {
-    const lorem = `Lorem Ipsum is simply dummy text of the printing
-                  and typesetting industry. Lorem Ipsum has been the
-                  industry's standard dummy text ever since the 1500s, \
-                  when an unknown printer took a galley of type and scrambled \
-                  it to make a type specimen book. It has survived not only \
-                  five centuries, but also the leap into electronic typesetting, \
-                  remaining essentially unchanged.`;
-    let projects = [
-      {
-        key: "ryan-lett-home-page",
-        title: "Home Page",
-        subtitle: "Foo",
-        body: lorem,
-        type: "react",
-        stack: [
-          "react",
-          "css-grid",
-          "semantic-html",
-        ]
-      },
-      {
-        key: "analytics-server",
-        title: "Analytics Server",
-        subtitle: "Foo",
-        body: lorem,
-        type: "php",
-      },
-      {
-        key: "image-proxy",
-        title: "Image Proxy",
-        subtitle: "Foo",
-        body: lorem,
-        type: "golang",
-      },
-    ];
-
-    return projects;
-  }
-
   constructor(props) {
     super(props)
     this.state = {
-      projects: this.getProjectData(),
+      error: null,
+      isLoaded: false,
+      projects: [],
     }
+  }
+
+  componentDidMount() {
+    fetch('/data/projects.json')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            projects: result.projects,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: false,
+            error
+          });
+        }
+      )
   }
 
   render() {
